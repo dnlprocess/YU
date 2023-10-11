b390569ab9c4b42e88d5bc2798fb47a01e2f1e91
daniellipshitz <dlipshit@mail.yu.edu>
2023-10-10 23:11:15 -0400

    BigOIt2 it1

--- IntroToAlgorithms/assignments/BigOIt2/src/main/java/edu/yu/introtoalgs/BigOIt2.java
+++ IntroToAlgorithms/assignments/BigOIt2/src/main/java/edu/yu/introtoalgs/BigOIt2.java
@@ -61,7 +61,8 @@ public class BigOIt2 extends BigOIt2Base {
      */
 
     public double doublingRatio(String bigOMeasurable, long timeOutInMs) throws IllegalArgumentException {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        long startTime = System.nanoTime();
+        ExecutorService executor = Executors.newFixedThreadPool(4);
         Timer timer = new Timer(timeOutInMs);
 
         List<Integer> nValues = new ArrayList<>();
@@ -70,9 +71,15 @@ public class BigOIt2 extends BigOIt2Base {
         List<Double> slopes = new ArrayList<>();
         List<Double> ratios = new ArrayList<>();
 
-        BigOMeasurable oMeasurable;
+        BigOMeasurable oMeasurable1;
+        BigOMeasurable oMeasurable2;
+        BigOMeasurable oMeasurable3;
+        BigOMeasurable oMeasurable4;
         try {
-            oMeasurable = (BigOMeasurable) Class.forName(bigOMeasurable).getConstructor().newInstance();
+            oMeasurable1 = (BigOMeasurable) Class.forName(bigOMeasurable).getConstructor().newInstance();
+            oMeasurable2 = (BigOMeasurable) Class.forName(bigOMeasurable).getConstructor().newInstance();
+            oMeasurable3 = (BigOMeasurable) Class.forName(bigOMeasurable).getConstructor().newInstance();
+            oMeasurable4 = (BigOMeasurable) Class.forName(bigOMeasurable).getConstructor().newInstance();
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
@@ -80,26 +87,48 @@ public class BigOIt2 extends BigOIt2Base {
         try {
             timer.start();
 
+            //warm-up
+            for (int i=0; i < 10; i++) {
+                oMeasurable1.setup(5);
+                oMeasurable1.execute();
+            }
+
             while (!timer.isTimedOut()) {
                 int n = nValues.isEmpty()? 8: 2 * nValues.get(nValues.size()-1);
 
                 Callable<Double> testTask1 = () -> {
-                    oMeasurable.setup(n);
+                    oMeasurable1.setup(n);
                     long start = System.nanoTime();
-                    oMeasurable.execute();
+                    oMeasurable1.execute();
                     long end = System.nanoTime();
                     return (double) (end - start);
                 };
                 Callable<Double> testTask2 = () -> {
-                    oMeasurable.setup(n);
+                    oMeasurable2.setup(n);
                     long start = System.nanoTime();
-                    oMeasurable.execute();
+                    oMeasurable2.execute();
+                    long end = System.nanoTime();
+                    return (double) (end - start);
+                };
+                Callable<Double> testTask3 = () -> {
+                    oMeasurable3.setup(n+1);
+                    long start = System.nanoTime();
+                    oMeasurable3.execute();
+                    long end = System.nanoTime();
+                    return (double) (end - start);
+                };
+                Callable<Double> testTask4 = () -> {
+                    oMeasurable4.setup(n-1);
+                    long start = System.nanoTime();
+                    oMeasurable4.execute();
                     long end = System.nanoTime();
                     return (double) (end - start);
                 };
 
                 Future<Double> testFuture1 = executor.submit(testTask1);
                 Future<Double> testFuture2 = executor.submit(testTask2);
+                Future<Double> testFuture3 = executor.submit(testTask3);
+                Future<Double> testFuture4 = executor.submit(testTask4);
 
                 if (timer.isTimedOut()) {
                     break;
@@ -107,38 +136,44 @@ public class BigOIt2 extends BigOIt2Base {
 
                 nValues.add(n);
                 try {
-                    runTimes.add((testFuture1.get() + testFuture2.get())/2);
+                    runTimes.add((testFuture1.get() + testFuture2.get() + testFuture3.get() + testFuture4.get())/4);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 } catch (ExecutionException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    // could be outofmemory
+                    break;
                 }
 
-
-                double powerMSE = calculateSlope(nValues, runTimes, slopes);
-
-                //ratios
-                
                 if (runTimes.size() < 4) {
                     ratios.add(Double.NaN);
                     continue;
                 }
-                while (ratios.remove(Double.NaN)){};
+                
                 ratios.add(runTimes.get(runTimes.size()-1)/runTimes.get(runTimes.size()-2));
-                if (ratios.get(ratios.size()-1) < 1) {// decreasing despite doubling n
+        
+                
+                if (ratios.get(ratios.size()-1) < 1 && runTimes.get(runTimes.size()-2)/runTimes.get(runTimes.size()-3) < 1) {// decreasing despite doubling n
+                    System.out.printf("error: %.2f, runtime less: %.2f, runtime more: %.2f", ratios.get(ratios.size()-1), runTimes.get(runTimes.size()-2), runTimes.get(runTimes.size()-1));
                     throw new IllegalArgumentException();
                 }
+        
+                System.out.println(ratios.get(ratios.size()-1));
                 
-
+                if (runTimes.size() < 6) {
+                    continue;
+                }
+                //double powerMSE = calculateSlope(nValues, runTimes, slopes);
+                
+                while (ratios.remove(Double.NaN)){};
+                /*double zScore = 1;
                 double meanRuntime = runTimes.stream().mapToDouble(Double::doubleValue).average().orElse(1.0);
-                double std = Math.sqrt(runTimes.stream().map(i -> Math.pow(i-meanRuntime, 2)).mapToDouble(Double::doubleValue).sum()/n);
-                if (Math.abs(meanRuntime - runTimes.get(0)) > std) {
+                double std = Math.sqrt(runTimes.stream().map(i -> Math.pow(i-meanRuntime, 2)).mapToDouble(Double::doubleValue).sum()/(n-1));
+                if (Math.abs(meanRuntime - runTimes.get(0)) > std * zScore) {
                     nValues.remove(0);
                     runTimes.remove(0);
                     ratios.remove(0);
-                    slopes.remove(0);
+                    //slopes.remove(0);
                 }
 
                 double ratioMSE = IntStream.range(1, runTimes.size())
@@ -148,28 +183,50 @@ public class BigOIt2 extends BigOIt2Base {
                 if (ratioMSE > powerMSE) {
                     ratios.remove(ratios.size()-1);
                     ratios.add(slopes.get(slopes.size()-1));
-                }
+                }*/
             }
 
         }
         finally {
             executor.shutdown();
         }
-        return ratios.stream().mapToDouble(Double::doubleValue).average().orElse(1.0);
+        double ratio = ratios.stream().mapToDouble(Double::doubleValue).average().orElse(1.0);
+        double meanRuntime = runTimes.stream().mapToDouble(Double::doubleValue).average().orElse(1.0);
+        double std = Math.sqrt(runTimes.stream().map(i -> Math.pow(i-meanRuntime, 2)).mapToDouble(Double::doubleValue).sum()/(runTimes.size()-1));
+        if (runTimes.size() < 4 || std/meanRuntime > 4) {
+            System.out.printf("Size is %d, and rsd is %.2f", runTimes.size(), std/meanRuntime);
+            return Double.NaN;
+        }
+        long endTime = System.nanoTime();
+        
+        System.out.printf("Size is %d, and rsd is %.2f", runTimes.size(), std/meanRuntime);
+            
+        //System.out.printf("ratio: %.2f, slope: %.2f", ratios.get(ratios.size()-1), slopes.get(slopes.size()-1));
+        System.out.println("ratios");
+        ratios.stream().forEach(element -> System.out.print(element + "\t"));
+        System.out.println("\nslopes");
+        slopes.stream().forEach(element -> System.out.print(element + "\t"));
+        System.out.printf("n: %d", nValues.get(nValues.size()-1));
+        System.out.printf("\n ratio avg: %.2f", ratio);
+        System.out.printf("time: %.2f", (double) (endTime-startTime));
+        return ratio;
         //return Math.pow(2, slopes.get(slopes.size()-1));
     }
 
     private double calculateSlope(List<Integer> nValues, List<Double> runTimes, List<Double> slopes) {
-        int n = nValues.size();
-        List<Double> logX = nValues.stream().map(i -> Math.log(i)).collect(Collectors.toList());
-        List<Double> logY = runTimes.stream().map(i -> Math.log(i)).collect(Collectors.toList());
-
+        List<Integer> x = nValues.stream().skip(4).collect(Collectors.toList());
+        List<Double> y = runTimes.stream().skip(4).collect(Collectors.toList());
+        
+        List<Double> logX = x.stream().map(i -> Math.log(i)).collect(Collectors.toList());
+        List<Double> logY = y.stream().map(i -> Math.log(i)).collect(Collectors.toList());
+        int n = logX.size();
         //Use linear regression with logs of values as trying to fit to power law
         //Formuals from: https://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html
 
         double sumN = logX.stream().mapToDouble(Double::doubleValue).sum();
         double sumTimes = logY.stream().mapToDouble(Double::doubleValue).sum();
         double sumNSquared = logX.stream().map(i -> i*i).mapToDouble(Double::doubleValue).sum();
+        System.out.printf("nvalue size: %d, runtime size: %d", nValues.size(), runTimes.size());
         double sumNRuntimes = IntStream.range(0, n)
         .mapToObj(i -> logX.get(i)*logY.get(i)).mapToDouble(Double::doubleValue).sum();
 
@@ -186,17 +243,14 @@ public class BigOIt2 extends BigOIt2Base {
 
         double multiplicativeConstant = (sumTimes - (power * sumN)) / n;
 
-        List<Double> predRuntimes = nValues.stream().map(i -> multiplicativeConstant * Math.pow(i, power)).collect(Collectors.toList());
+        List<Double> predRuntimes = x.stream().map(i -> multiplicativeConstant * Math.pow(i, power)).collect(Collectors.toList());
 
         List<Double> rss = IntStream.range(0, n)
-        .mapToObj(i -> Math.pow(runTimes.get(i) - predRuntimes.get(i), 2))
+        .mapToObj(i -> Math.pow(y.get(i) - predRuntimes.get(i), 2))
         .collect(Collectors.toList());
 
         double mse = rss.stream().mapToDouble(Double::doubleValue).sum()/n;
         
-        if (mse >= 1) {
-            return Double.NaN;
-        }
         return mse;
     }
 }
--- IntroToAlgorithms/assignments/BigOIt2/src/test/java/edu/yu/introtoalgs/AppTest.java
+++ IntroToAlgorithms/assignments/BigOIt2/src/test/java/edu/yu/introtoalgs/AppTest.java
@@ -1,6 +1,12 @@
 package edu.yu.introtoalgs;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -13,8 +19,43 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        BigOIt2 algTester = new BigOIt2();
+
+        try {
+            BigOMeasurable toBeTested = (BigOMeasurable) Class.forName("edu.yu.introtoalgs.DoubleLoop").getConstructor().newInstance();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        double ratio = algTester.doublingRatio("edu.yu.introtoalgs.DoubleLoop", 60000);
+        assertEquals(4.0, ratio, 0.1);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        BigOIt2 algTester = new BigOIt2();
+
+        double ratio = algTester.doublingRatio("edu.yu.introtoalgs.SumOfSquares", 60000);
+        assertEquals(4.0, ratio, 0.1);
     }
 }
