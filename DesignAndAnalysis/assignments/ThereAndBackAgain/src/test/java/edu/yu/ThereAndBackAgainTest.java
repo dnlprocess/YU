35d3b0bb1c85fd9be03cb6cdada54900f8ad29bd
daniellipshitz <dlipshit@mail.yu.edu>
2024-02-05 08:26:54 -0500

    Proper folder

--- DesignAndAnalysis/assignments/ThereAndBackAgain/src/test/java/edu/yu/ThereAndBackAgainTest.java
+++ DesignAndAnalysis/assignments/ThereAndBackAgain/src/test/java/edu/yu/ThereAndBackAgainTest.java
@@ -9,8 +9,6 @@ import java.util.Random;
 
 import org.junit.Assert;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
